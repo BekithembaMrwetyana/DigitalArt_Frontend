dfb066f66cd0114de94943e7da751767ab4006f4
Mpilonhle Zimela Mzimela <230197833@mycput.ac.za>
2025-09-15 23:59:48 +0200

    removed default user

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store'
 import './assets/styles/main.css'
 
 // Simulate logged-in user for testing MyOrders page
-const testUser = { userId: 15, firstName: 'Thando', lastName: 'Mseleku' }
-localStorage.setItem('user', JSON.stringify(testUser))
+//const testUser = { userId: 15, firstName: 'Thando', lastName: 'Mseleku' }
+//localStorage.setItem('user', JSON.stringify(testUser))
 
 const app = createApp(App)
 app.use(store)
